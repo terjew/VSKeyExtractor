22df78216a92e1c9c0d765a0e8b7d24e0bfc68b6
Terje Wiesener <wiesener@gmail.com>
2021-06-21 00:08:56 +0200

    Remove record syntax

--- Program.cs
+++ Program.cs
@@ -7,7 +7,18 @@ using System.Text.RegularExpressions;
 
 namespace VSKeyExtractor
 {
-    record Product(string Name, string GUID, string MPC);
+    struct Product
+    {
+        public string Name { get; }
+        public string GUID { get; }
+        public string MPC { get; }
+        public Product(string Name, string GUID, string MPC)
+        {
+            this.Name = Name;
+            this.GUID = GUID;
+            this.MPC = MPC;
+        }
+    }
 
     class Program
     {
