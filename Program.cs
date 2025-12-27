ecb090a2ca0c4c4af09e75cbe74ffea4650401f9
Joe Care <Joe_care_99@yahoo.com>
2025-12-27 10:10:56 +0100

    Projekt auf .NET 4.8.1 aktualisiert, Produkte & Code verbessert
    Projekt auf .NET Framework 4.8.1 angehoben und app.config hinzugefügt. Produktliste um VS 2022 erweitert, GUIDs als Guid-Objekte gespeichert und Product zu struct geändert. Lizenzextraktion robuster gemacht (Unicode, Debug-Ausgaben). Projekt-GUIDs und Bootstrapper/Deployment-Einstellungen angepasst.

--- Program.cs
+++ Program.cs
@@ -1,41 +1,56 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 
 namespace VSKeyExtractor
 {
-    record Product(string Name, string GUID, string MPC);
+    internal struct Product
+    {
+        public string Name;
+        public Guid GUID;
+        public string MPC;
+
+        public Product(string item1, Guid item2, string item3)
+        {
+            Name = item1;
+            GUID = item2;
+            MPC = item3;
+        }
+    }
 
     class Program
     {
-        static readonly List<Product> Products = new List<Product>
+        private static readonly List<Product> products = new List<Product>()
         {
-            new Product("Visual Studio Express 2012 for Windows Phone"  , "77550D6B-6352-4E77-9DA3-537419DF564B", "04937"),
-            new Product("Visual Studio Professional 2012"               , "77550D6B-6352-4E77-9DA3-537419DF564B", "04938"),
-            new Product("Visual Studio Ultimate 2012"                   , "77550D6B-6352-4E77-9DA3-537419DF564B", "04940"),
-            new Product("Visual Studio Premium 2012"                    , "77550D6B-6352-4E77-9DA3-537419DF564B", "04941"),
-            new Product("Visual Studio Test Professional 2012"          , "77550D6B-6352-4E77-9DA3-537419DF564B", "04942"),
-            new Product("Visual Studio Express 2012 for Windows Desktop", "77550D6B-6352-4E77-9DA3-537419DF564B", "05695"),
+            new Product("Visual Studio Express 2012 for Windows Phone"  , new Guid("77550D6B-6352-4E77-9DA3-537419DF564B"), "04937"),
+            new Product("Visual Studio Professional 2012"               , new Guid("77550D6B-6352-4E77-9DA3-537419DF564B"), "04938"),
+            new Product("Visual Studio Ultimate 2012"                   , new Guid("77550D6B-6352-4E77-9DA3-537419DF564B"), "04940"),
+            new Product("Visual Studio Premium 2012"                    , new Guid("77550D6B-6352-4E77-9DA3-537419DF564B"), "04941"),
+            new Product("Visual Studio Test Professional 2012"          , new Guid("77550D6B-6352-4E77-9DA3-537419DF564B"), "04942"),
+            new Product("Visual Studio Express 2012 for Windows Desktop", new Guid("77550D6B-6352-4E77-9DA3-537419DF564B"), "05695"),
 
-            new Product("Visual Studio 2013 Professional"               , "E79B3F9C-6543-4897-BBA5-5BFB0A02BB5C", "06177"),
-            new Product("Visual Studio 2013 Ultimate"		            , "E79B3F9C-6543-4897-BBA5-5BFB0A02BB5C", "06181"),
-            
-            new Product("Visual Studio 2015 Enterprise"                 , "4D8CFBCB-2F6A-4AD2-BABF-10E28F6F2C8F", "07060"),
-            new Product("Visual Studio 2015 Professional"               , "4D8CFBCB-2F6A-4AD2-BABF-10E28F6F2C8F", "07062"),
+            new Product("Visual Studio 2013 Professional"               , new Guid("E79B3F9C-6543-4897-BBA5-5BFB0A02BB5C"), "06177"),
+            new Product("Visual Studio 2013 Ultimate"                   , new Guid("E79B3F9C-6543-4897-BBA5-5BFB0A02BB5C"), "06181"),
 
-            new Product("Visual Studio 2017 Enterprise"                 , "5C505A59-E312-4B89-9508-E162F8150517", "08860"),
-            new Product("Visual Studio 2017 Professional"               , "5C505A59-E312-4B89-9508-E162F8150517", "08862"),
-            new Product("Visual Studio 2017 Test Professional"          , "5C505A59-E312-4B89-9508-E162F8150517", "08866"),
+            new Product("Visual Studio 2015 Enterprise"                 , new Guid("4D8CFBCB-2F6A-4AD2-BABF-10E28F6F2C8F"), "07060"),
+            new Product("Visual Studio 2015 Professional"               , new Guid("4D8CFBCB-2F6A-4AD2-BABF-10E28F6F2C8F"), "07062"),
 
-            new Product("Visual Studio 2019 Enterprise"                 , "41717607-F34E-432C-A138-A3CFD7E25CDA", "09260"),
-            new Product("Visual Studio 2019 Professional"               , "41717607-F34E-432C-A138-A3CFD7E25CDA", "09262"),
+            new Product("Visual Studio 2017 Enterprise"                 , new Guid("5C505A59-E312-4B89-9508-E162F8150517"), "08860"),
+            new Product("Visual Studio 2017 Professional"               , new Guid("5C505A59-E312-4B89-9508-E162F8150517"), "08862"),
+            new Product("Visual Studio 2017 Test Professional"          , new Guid("5C505A59-E312-4B89-9508-E162F8150517"), "08866"),
 
-            new Product("Visual Studio 2022 Enterprise"                 , "1299B4B9-DFCC-476D-98F0-F65A2B46C96D", "09660"),
-            new Product("Visual Studio 2022 Professional"               , "1299B4B9-DFCC-476D-98F0-F65A2B46C96D", "09662"),
+            new Product("Visual Studio 2019 Enterprise"                 , new Guid("41717607-F34E-432C-A138-A3CFD7E25CDA"), "09260"),
+            new Product("Visual Studio 2019 Professional"               , new Guid("41717607-F34E-432C-A138-A3CFD7E25CDA"), "09262"),
+
+            new Product("Visual Studio 2022 Enterprise"                 , new Guid("1299B4B9-DFCC-476D-98F0-F65A2B46C96D"), "09660"),
+            new Product("Visual Studio 2022 Professional"               , new Guid("1299B4B9-DFCC-476D-98F0-F65A2B46C96D"), "09662"),
+
         };
+        static readonly List<Product> Products = products;
 
         static void Main()
         {
@@ -49,19 +64,19 @@ namespace VSKeyExtractor
             try
             {
                 var secret = ProtectedData.Unprotect((byte[])encrypted, null, DataProtectionScope.CurrentUser);
-                var unicode = new UnicodeEncoding();
-                var str = unicode.GetString(secret);
+                var str = Encoding.Unicode.GetString(secret);
                 foreach (var sub in str.Split('\0'))
-                {
-                    var match = Regex.Match(sub, @"\w{5}-\w{5}-\w{5}-\w{5}-\w{5}");
-                    if (match.Success)
+                    if (!string.IsNullOrWhiteSpace(sub))
                     {
-                        Console.WriteLine($"Found key for {product.Name}: {match.Captures[0]}");
+                        Debug.WriteLine($"sub: {sub}");
+                        var match = Regex.Match(sub, @"\w{5}-\w{5}-\w{5}-\w{5}-\w{5}");
+                        if (match.Success)
+                        {
+                            Console.WriteLine($"Found key for {product.Name}: {match.Captures[0]}");
+                        }
                     }
-                }
             }
             catch (Exception) {/*just void em*/}
         }
     }
 }
-
