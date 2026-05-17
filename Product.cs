5a599df1fc88edbd10026f9e50a0e6805791d946
Joe Care <Joe_care_99@yahoo.com>
2026-05-17 11:31:12 +0200

    Save Product split before upgrade

---
+++ Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VSKeyExtractor
+{
+    internal struct Product
+    {
+        public string Name { get; }
+        public Guid GUID { get; }
+        public string MPC { get; }
+        public Product(string name, Guid guid, string mpc)
+        {
+            Name = name;
+            GUID = guid;
+            MPC = mpc;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,18 +8,6 @@ using System.Text.RegularExpressions;
 
 namespace VSKeyExtractor
 {
-    internal struct Product
-    {
-        public string Name { get; }
-        public Guid GUID { get; }
-        public string MPC { get; }
-        public Product(string name, Guid guid, string mpc)
-        {
-            Name = name;
-            GUID = guid;
-            MPC = mpc;
-        }
-    }
 
     class Program
     {
